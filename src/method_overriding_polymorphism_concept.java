65d07eb68d16cb434658346c12d0a0aae488b2e2
srivatsadavuluriiii <xstcy@protonmail.com>
2024-06-16 00:46:53 +0530

    null commit

--- src/method_overriding_polymorphism_concept.java
+++ src/method_overriding_polymorphism_concept.java
@@ -7,14 +7,14 @@ class animal {
     }
 }
 
-class dog extends animal {
+class doggo extends animal {
     @Override
     void sound() {
         System.out.println("Dog barks");
     }
 }
 
-class cat extends animal {
+class catto extends animal {
     @Override
     void sound() {
         System.out.println("Cat meows");
@@ -23,8 +23,8 @@ class cat extends animal {
 
 public class method_overriding_polymorphism_concept {
     public static void main(String[] args) {
-        animal object_dog = new dog();
-        animal object_cat = new cat();
+        animal object_dog = new doggo();
+        animal object_cat = new catto();
         object_dog.sound();
         object_cat.sound();
     }
