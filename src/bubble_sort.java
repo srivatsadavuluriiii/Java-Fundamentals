972db74ee8fcfa5efabdca0240299e7f475bea6a
srivatsadavuluriiii <xstcy@protonmail.com>
2024-06-16 16:10:01 +0530

    null commit

--- src/bubble_sort.java
+++ src/bubble_sort.java
@@ -8,8 +8,7 @@ public class bubble_sort {
         int[] arr = {1,2,3,8,7,5,6,4};
         int temp;
         System.out.println("Unsorted Array");
-        System.out.print("{");
-        for (int p : arr) { System.out.print(p + ", ");}System.out.println("}");
+        System.out.print("{");for (int p : arr) { System.out.print(p + ", ");}System.out.println("}");
 
         for(int i = arr.length - 1  ; i > 0  ; i--){
             for(int j = 0 ; j < i ; j++){
@@ -24,12 +23,8 @@ public class bubble_sort {
         System.out.println("Sorted Array");
         System.out.print("{");
         for (int p : arr) { System.out.print(p + ", ");}System.out.println("}");
-
-
-
-
     }
-    
 }
+
 
 
